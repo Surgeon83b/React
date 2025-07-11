f391b2c42084abc570f3de09ee7129b3f33716a4
Surgeon83b <89166001+Surgeon83b@users.noreply.github.com>
2025-07-11 15:35:46 +0300

    feat: add functionality

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Search from './components/Search.tsx';
+import Results from './components/Results.tsx';
+import { Component } from 'react';
+import type { Pokemon } from './types.ts';
+import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import ErrorButton from './components/ErrorButton.tsx';
+import ErrorPage from './components/ErrorPage.tsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface AppState {
+  data?: Pokemon[];
+  error: string;
 }
 
-export default App
+class App extends Component<Record<string, never>, AppState> {
+  state: AppState = {
+    data: [],
+    error: '',
+  };
+
+  onSearch = (newData: Array<Pokemon> | undefined, err: string) => {
+    this.setState({ data: newData, error: err });
+  };
+
+  render() {
+    return (
+      <ErrorBoundary fallback={<ErrorPage />}>
+        <div className='flex wrapper full'>
+          <div className='info'>
+            <Search onSearch={this.onSearch} />
+            <Results data={this.state.data} error={this.state.error} />
+          </div>
+          <ErrorButton />
+        </div>
+      </ErrorBoundary>
+    );
+  }
+}
+
+export default App;
