f321548997a389a0d68d4f49f430bb283df80fe0
Surgeon83b <89166001+Surgeon83b@users.noreply.github.com>
2025-07-23 14:17:35 +0300

    refactor: convert App into functional Component

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,41 +1,30 @@
 import './App.css';
 import Search from './components/Search/Search.tsx';
 import Results from './components/Results/Results.tsx';
-import { Component } from 'react';
-import type { Pokemon } from './types.ts';
+import { useState } from 'react';
+import type { SearchData } from './types.ts';
 import { ErrorBoundary } from './components/ErrorBoundary';
-import ErrorButton from './components/ErrorButton.tsx';
 import ErrorPage from './components/ErrorPage.tsx';
 
-interface AppState {
-  data?: Pokemon[];
-  error: string;
-}
+const App = () => {
+  const [data, setData] = useState<SearchData>([]);
+  const [error, setError] = useState('');
 
-class App extends Component<Record<string, never>, AppState> {
-  state: AppState = {
-    data: [],
-    error: '',
+  const onSearch = (newData: SearchData, err: string) => {
+    setData(newData);
+    setError(err);
   };
 
-  onSearch = (newData: Array<Pokemon> | undefined, err: string) => {
-    this.setState({ data: newData, error: err });
-  };
-
-  render() {
-
-    return (
-      <ErrorBoundary fallback={<ErrorPage />}>
-        <div className='flex wrapper full'>
-          <div className='info'>
-            <Search onSearch={this.onSearch} placeholder={'Search'} />
-            <Results data={this.state.data} error={this.state.error} />
-          </div>
-          <ErrorButton />
+  return (
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <div className='flex wrapper full'>
+        <div className='info'>
+          <Search onSearch={onSearch} placeholder={'Search'} />
+          <Results data={data} error={error} />
         </div>
-      </ErrorBoundary>
-    );
-  }
-}
+      </div>
+    </ErrorBoundary>
+  );
+};
 
 export default App;
