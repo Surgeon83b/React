97135674e4114ae1f90de009436ab539bdc2c4f7
Surgeon83b <89166001+Surgeon83b@users.noreply.github.com>
2025-08-01 10:21:52 +0300

    feature: add CloseButton to About page

--- class-components/src/pages/About.tsx
+++ class-components/src/pages/About.tsx
@@ -1,6 +1,11 @@
+import {useNavigate} from "react-router";
+
 export const About = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={'flex centered column'}>
+      <div className="close" onClick={()=>navigate(-1)}></div>
       <h3>author: Kanstantsin Piatkevich</h3>
       <h4> phone: +37529 7778687</h4>
       <a href='https://rs.school/courses/reactjs'>Join the next React Course</a>
